a77ff2e7f08ee005333c7b3375f01301a88f49d0
Glacielar <glacielarwayua@gmail.com>
2023-06-26 20:44:53 +0000

    t0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    try:
-        for element in my_list:
-            if count < x:
-                print(element, end=" ")
-                count += 1
-            else:
-                break
-    except TypeError:
-        print("Error: Unable to print element due to a type error.")
-    finally:
-        print()
-    return count
+    """Print x elememts of a list.
+
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
+
+    Returns:
+        The number of elements printed.
+    """
+    ret = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            ret += 1
+        except IndexError:
+            break
+    print("")
+    return (ret)
