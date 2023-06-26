bc4ceb4c3a057f032760db2d104a4c7a26009cfc
Glacielar <glacielarwayua@gmail.com>
2023-06-26 20:28:30 +0000

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for element in my_list:
+            if count < x:
+                print(element, end=" ")
+                count += 1
+            else:
+                break
+    except TypeError:
+        print("Error: Unable to print element due to a type error.")
+    finally:
+        print()
+    return count
